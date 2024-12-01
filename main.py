777165b15c3ab691502674161d0081f329696c04
PaketOs1nt <duga228337@gmail.com>
2024-12-01 11:36:12 +0200

    lolol

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
 import os, random
+import socket
+
+class MegaNetworkHackerProUltraMicrosoftAppleHackForFree:
+    def __init__(self, port: int):
+        self.port = port
+
+    def create_mega_ultra_hack_server(self):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.bind(('0.0.0.0', self.port))
+        return sock
+    
+    def start_mega_server(self, sock: socket.socket, max_clients: int):
+        sock.listen(max_clients)
+    
+    def log_clifsefmnsdnfmsdnfmsdf_on_mega_server(self, sock: socket.socket):
+        while True:
+            clsock, addr = sock.accept()
+            print(f'OMAGAd new cliqermwnssd;af!!!!! ip: {addr[0]}!!!!! go to dgs bobd osent kids asdiasdiaidsd.l;kslakka!')
+            clsock.close()
+
+def main():
+    sadassdadsa = MegaNetworkHackerProUltraMicrosoftAppleHackForFree(1487)
+    seraver = sadassdadsa.create_mega_ultra_hack_server()
+    sadassdadsa.start_mega_server(seraver, max_clients=123)
+    sadassdadsa.log_clifsefmnsdnfmsdnfmsdf_on_mega_server(seraver)
+
+main()
