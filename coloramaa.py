071c660deeb03958bb26dd115117ad88fc325491
PaketOs1nt <duga228337@gmail.com>
2024-12-21 20:28:12 +0200

    sadwqedsadsa

---
+++ coloramaa.py
@@ -0,0 +1,14 @@
+import colorama
+
+colorama.init(autoreset=True)
+
+table = {
+    'colorama.init': "Дуже важлива функцiя для того щоб модуль працював нормально, по словам ChatGPT - патчить консоль (якщо на windows) для того щоб вона пiдтримувала ANSI",
+    'colorama.Fore/Back': "Списки ANSI кодiв для того щоб мiняти кольор тексту або фону у деяких участках",
+    'colorama.Cursor': "Функкцiя для управлiнням курсором ввода у консолi"
+}
+
+for obj, text in table.items():
+    print(f"{colorama.Fore.GREEN}[{obj}]:\n{colorama.Fore.LIGHTGREEN_EX}{text}\n\n")
+
+input(colorama.Fore.YELLOW + 'На мою думку це найважливiшi компненти для користувача бiблiотеки colorama')
